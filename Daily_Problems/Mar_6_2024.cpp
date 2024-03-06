cef49196377e033bba4ba44219794f3f71de2c7e
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-03-06 11:39:47 +0530

    Create Mar_6_2024.cpp

---
+++ Daily_Problems/Mar_6_2024.cpp
@@ -0,0 +1,11 @@
+vector<int> search(string pattern, string text)
+        {
+            vector<int> ans;
+            int pos = text.find(pattern, 0);
+            while (pos != string::npos) 
+            {
+                ans.push_back(pos+1);
+                pos = text.find(pattern, pos + 1);
+            }
+            return ans;
+        }
