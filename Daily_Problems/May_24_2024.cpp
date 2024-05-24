54673c8f221b203d82564244d697bd5b1644b23a
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-05-24 11:14:53 +0530

    Create May_24_2024.cpp

---
+++ Daily_Problems/May_24_2024.cpp
@@ -0,0 +1,28 @@
+int mod=1e9+7;
+  int solve(int n,vector<int>&arr,vector<vector<int>>&dp,long long sum){
+      
+      if(n==0)
+      return sum==0;
+      
+      if(dp[sum][n]!=-1)
+      return dp[sum][n];
+      
+      if(arr[n-1]<=sum)
+      return dp[sum][n]=(solve(n-1,arr,dp,sum-arr[n-1])%mod+solve(n-1,arr,dp,sum)%mod)%mod;
+      else
+      return dp[sum][n]=solve(n-1,arr,dp,sum)%mod;
+      
+  }
+    int countPartitions(int n, int d, vector<int>& arr) {
+        
+        int sum=accumulate(arr.begin(),arr.end(),0);
+        
+        if((sum+d)%2)
+        return 0;
+        
+        sum=((sum+d)/2)%mod;
+        
+        vector<vector<int>>dp(sum+1,vector<int>(n+1,-1));
+        
+        return solve(n,arr,dp,sum)%mod;
+    }
