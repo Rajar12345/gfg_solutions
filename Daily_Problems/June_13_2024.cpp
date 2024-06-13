21cdabfc2e76d34503cb16e9cbea0cc12a5c12d1
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-06-13 09:09:39 +0530

    Create June_13_2024.cpp

---
+++ Daily_Problems/June_13_2024.cpp
@@ -0,0 +1,18 @@
+#define MOD 1000000007
+
+class Solution
+{
+    public:
+    int padovanSequence(int n)
+    {
+       vector<int> dp(n+1, -1);
+       dp[0] = dp[1] = dp[2] = 1;
+       
+       for(int i=3;i<=n;i++){
+           dp[i] = dp[i-2]%(MOD)+  dp[i-3]%(MOD);
+       }
+       
+       return dp[n]%(MOD);
+    }
+    
+};
