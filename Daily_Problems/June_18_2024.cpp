abb6aaaa54b6245eab2e79e1dc7e9753731a4dbb
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-06-18 08:41:07 +0530

    Create June_18_2024.cpp

---
+++ Daily_Problems/June_18_2024.cpp
@@ -0,0 +1,12 @@
+int rectanglesInCircle(int r) {
+        int ans=0;
+        for(int i=1;i<2*r;i++)
+        {
+            for(int j=1;j<2*r;j++)
+            {
+                if(sqrt(i*i+j*j)<=2*r)
+                    ans++;
+            }
+        }
+        return ans;
+    }
