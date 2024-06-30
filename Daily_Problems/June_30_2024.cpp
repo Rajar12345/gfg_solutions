4da2a35f0e77bf7437f0011385487e58b5a31ab7
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-06-30 10:17:08 +0530

    Create June_30_2024.cpp

---
+++ Daily_Problems/June_30_2024.cpp
@@ -0,0 +1,23 @@
+Node* deleteNode(Node* head, int x) {
+        // Your code here
+        Node* temp = head;
+        Node* curr = NULL;
+        if(x == 1)
+        {
+            temp = temp->next;
+            temp->prev = NULL;
+            return temp;
+        }
+        int cnt = 1;
+        while(cnt < x)
+        {
+            curr = temp;
+            temp = temp->next;
+            cnt++;
+        }
+        curr->next = temp->next;
+        temp->next = NULL;
+        temp->prev = NULL;
+        delete temp;
+        return head;
+    }
