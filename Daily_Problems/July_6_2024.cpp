7e27703e12d138eb8fbddd54d8487abb4d98b2dc
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-07-06 10:47:35 +0530

    Create July_6_2024.cpp

---
+++ Daily_Problems/July_6_2024.cpp
@@ -0,0 +1,13 @@
+Node* pre=NULL;
+    void populateNext(Node *root) {
+        // code here
+        if(root==NULL){
+            return;
+        }
+        populateNext(root->left);
+        if(pre!=NULL){
+            pre->next=root;
+        }
+        pre=root;
+        populateNext(root->right);
+    }
