293bf18e43bfc4931c248067adb007b3d6317d87
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-07-21 11:51:10 +0530

    Create July_21_2024.cpp

---
+++ Daily_Problems/July_21_2024.cpp
@@ -0,0 +1,17 @@
+sort(arr.begin(),arr.end());
+        int n=arr.size();
+        long long int prod=1;
+        int mini=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(arr[i]<0){
+                mini=max(arr[i],mini);
+            }
+            if(arr[i]!=0){
+                prod=prod*arr[i];
+                prod%=1000000007;
+            }
+        }
+       if(prod<0){
+           prod=prod/mini;
+       }
+       return prod;
