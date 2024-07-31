1684b9e88ccb51d2a57e0e0c797aeec97e8dc21d
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-07-31 13:17:17 +0530

    Create July_31_2024.cpp

---
+++ Daily_Problems/July_31_2024.cpp
@@ -0,0 +1,12 @@
+string ans = "";
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<arr[0].size(); i++){
+            if(arr[0][i] == arr[arr.size()-1][i]){
+                ans.push_back(arr[0][i]);
+                //cout<<arr[0][i];
+            }
+            else{
+                break;
+            }
+        }
+        return ans.size()<=0 ? "-1" : ans;
